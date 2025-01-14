44a02e745117fe95b6cf4929589f1871626379f8
David Ward <dward@semquest.com>
2025-01-13 16:48:50 -0800

    port 10000 and ip zeros

--- first.py
+++ first.py
@@ -20,4 +20,4 @@ def hello_world():
 if __name__ == "__main__":
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run()
+    app.run(port=10000, host="0.0.0.0")
